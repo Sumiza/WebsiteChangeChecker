854a32cd38eef0e6be51fabed4a23fd16beeabb0
Sumiza <64884509+Sumiza@users.noreply.github.com>
2021-03-04 02:16:38 -0500

    Update mailjetapi.sh

--- mailjetapi.sh
+++ mailjetapi.sh
@@ -5,10 +5,13 @@
 apikey=""
 apipass=""
 
+#--- temporary workaround
+message=$(echo "$4" | tr '\n' ' ')
+#---
 curl -X POST --user "$apikey:$apipass" https://api.mailjet.com/v3/send \
 	-H 'Content-Type: application/json' \
 	-d '{	"FromEmail":'\""$1"\"',
 		"FromName":'\""$2"\"',
 		"Subject":'\""$3"\"',
-		"Text-part":'\""$4"\"',
+		"Text-part":'\""$message"\"',
 		"Recipients":[{"Email":'\""$5"\"'}]}'
